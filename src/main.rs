285d14598aeeeaa50c03236350ebadd49bcdec36
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-19 16:20:33 -0400

    Refactor core logic into lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
+
+use bootleg_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,35 +12,8 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = bootleg_grep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // Check num of args
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
 }
