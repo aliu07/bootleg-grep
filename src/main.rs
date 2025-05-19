f3689a702b9df4edeed33c0a233fafe1e06ebad8
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-19 17:08:42 -0400

    Redirect error message to stderr stream

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use bootleg_grep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = bootleg_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
